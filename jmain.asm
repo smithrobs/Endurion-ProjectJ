3f7d5e2bbd858b8e9c1eca47f486f7e07127613a
Robert Smith <smith.rob.s@gmail.com>
2013-04-20 12:00:00 +0000

    Here we go with step 2:
    In the first part we prepared everything for the VIC, now we set up our modified charset. Note that I used a selfmade tool (similar to CharPad). The file J.CHR contains the charset and is included into the source as binary. Since the C64 can't load files to locations after  we have to copy the charset to the target memory at  (and switch off the ROM therefore).
    
    The current step should display &quot;HELLO&quot;. The rest of the screen depends on the current memory setup of your emulator/C64.
    
    The actual game is currently at step 30, looking like this:
    
    The steps plan for now:
    
    Step1:
    Setting up VIC layout and main loop.
    
    Step2:
    Setting up custom charset and display &quot;Hello&quot;
    
    Step3:
    Setting up custom sprites and display
    
    Step4:
    Moving sprite with joystick
    
    Step5:
    Build stage screen
    
    Step6:
    Collision with screen
    
    Step7:
    Gravity/Jump
    
    Step8:
    Enemy placement
    
    Step9:
    Enemy movement
    
    Step10:
    Collision between objects and player
    
    Step11:
    Enemy kills player
    
    Step12:
    Player kills enemy
    
    Step13:
    Item placement
    
    Step14:
    Item collecting
    
    Step15:
    Level progress
    
    Step16:
    Score/Live/Level Display
    
    Step17:
    Gameplay refinement 1 (player movement and animation)
    
    Step18:
    Gameplay refinement 2 (enemy movement and animation)
    
    Step19:
    Gameplay refinement 3 (new enemy type, movement and animation)
    
    Step20:
    Gameplay refinement 4 (reload and item effects)
    
    Step21:
    Title Screen
    
    Step22:
    Highscores
    
    Step23:
    Highscores name entry
    
    Step24:
    Saving/Loading Highscores
    
    Step25:
    Title Logo
    
    Step26:
    Gameplay refinement 5 (dissolving/attacking enemy types)
    
    Step27:
    Gameplay refinement 6 (dissolving/attacking enemy types)
    
    Step28:
    enhanced level building types
    
    Step29:
    Title refinement (color fade)
    
    Step30:
    TODO Highscore entry refinement
    
    Step31:
    TODO More stages 1
    
    Step32:
    TODO Gameplay refinement 7 (more items)

--- jmain.asm
+++ jmain.asm
@@ -3,8 +3,23 @@
 
 ;define constants here
 
+;placeholder for various temp parameters
+PARAM1    = $03
+PARAM2    = $04
+PARAM3    = $05
+PARAM4    = $06
+PARAM5    = $07
+
+;placeholder for zero page pointers
+ZEROPAGE_POINTER_1 = $17
+ZEROPAGE_POINTER_2 = $19
+ZEROPAGE_POINTER_3 = $21
+ZEROPAGE_POINTER_4 = $23
+
 ;address of the screen buffer
 SCREEN_CHAR      = 52224
+;address of color ram
+SCREEN_COLOR        = $d800
 
 ;this creates a basic start
 *=$0801
@@ -26,12 +41,61 @@ SCREEN_CHAR      = 52224
           and #$fc
           sta 56576
 
+          ;----------------------
+          ;copy charset to target          
+          ;----------------------
+          
+          ;block interrupts 
+          ;since we turn ROMs off this would result in crashes if we didn't
+          sei
+          
+          ;save old configuration
+          lda $01
+          sta PARAM1
+          
+          ;only RAM
+          ;to copy under the IO rom
+          lda #%00110000
+          sta $01
+          
+          ;take source address from CHARSET
+          LDA #<CHARSET
+          STA ZEROPAGE_POINTER_1
+          LDA #>CHARSET
+          STA ZEROPAGE_POINTER_1 + 1
+          
+          ;now copy
+          jsr CopyCharSet
+
+          ;restore ROMs
+          lda PARAM1
+          sta $01
+          
+          cli
+          
+          ;test charset
+          lda #'H'
+          sta SCREEN_CHAR
+          lda #'E'
+          sta SCREEN_CHAR + 1
+          lda #'L'
+          sta SCREEN_CHAR + 2
+          sta SCREEN_CHAR + 3
+          lda #'O'
+          sta SCREEN_CHAR + 4
+          
+          lda #1
+          sta SCREEN_COLOR
+          sta SCREEN_COLOR + 1
+          sta SCREEN_COLOR + 2
+          sta SCREEN_COLOR + 3
+          sta SCREEN_COLOR + 4
+          
+
           ;the main game loop
 GameLoop  
           ;border flashing
           inc 53280
-          ;top left char
-          inc SCREEN_CHAR
           
           jsr WaitFrame
 
@@ -57,3 +121,47 @@ WaitFrame
           
           rts
 
+
+!zone CopyCharSet
+CopyCharSet
+          ;set target address ($F000)
+          lda #$00
+          sta ZEROPAGE_POINTER_2
+          lda #$F0
+          sta ZEROPAGE_POINTER_2 + 1
+
+          ldx #$00
+          ldy #$00
+          lda #0
+          sta PARAM2
+
+.NextLine
+          lda (ZEROPAGE_POINTER_1),Y
+          sta (ZEROPAGE_POINTER_2),Y
+          inx
+          iny
+          cpx #$08
+          bne .NextLine
+          cpy #$00
+          bne .PageBoundaryNotReached
+          
+          ;we've reached the next 256 bytes, inc high byte
+          inc ZEROPAGE_POINTER_1 + 1
+          inc ZEROPAGE_POINTER_2 + 1
+
+.PageBoundaryNotReached
+
+          ;only copy 254 chars to keep irq vectors intact
+          inc PARAM2
+          lda PARAM2
+          cmp #254
+          beq .CopyCharsetDone
+          ldx #$00
+          jmp .NextLine
+
+.CopyCharsetDone
+          rts
+
+
+CHARSET
+          !binary "j.chr"
